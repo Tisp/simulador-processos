3bd7c97b645af65a4a24fadbe69251344617bf31
thiago <thiago@thiagos-Mac-mini-5.local>
2016-08-16 18:13:56 -0300

    funcao que calcula primo, para gasto de cpu

---
+++ src/prime.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_prime(unsigned int num) {
+    
+    if(num <= 1) return 0;
+    
+    //Percorre os numero se nao encontrar e primo
+    for(int i = 2; i <= num / 2; i++) {
+        if((num % i) == 0) return 0;
+
+    }
+   
+    return 1; //é primo'
+}
