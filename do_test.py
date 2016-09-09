c75e407c069914558cb8646b2796e61c655d43c0
tisp <tisp.thiago@gmail.com>
2016-09-08 22:42:43 -0300

    arrumando python para testes

--- do_test.py
+++ do_test.py
@@ -1,23 +1,24 @@
 import subprocess
 
 for i in range(1, 4):
+	for t in range(1, 4):	
 
-	dados = '' 
-	media_deadline = 0.
-	media_contexto = 0.
+		dados = '' 
+		media_deadline = 0.
+		media_contexto = 0.
+	
+		for x in range(0, 30):
 
-	for x in range(0, 1):
-		deadline = subprocess.Popen('./simulador '+ str(i) +' tests/test_1 output  | grep -o Cumpriu | wc -l', shell=True, stdout=subprocess.PIPE).stdout.read()
-		contexto = subprocess.Popen('tail -n 1 output', shell=True, stdout=subprocess.PIPE).stdout.read()		
+			deadline = subprocess.Popen('./simulador '+ str(i) +' tests/test_'+ str(t) +' output  | grep -o Cumpriu | wc -l', shell=True, stdout=subprocess.PIPE).stdout.read()
+			contexto = subprocess.Popen('tail -n 1 output', shell=True, stdout=subprocess.PIPE).stdout.read()		
 
-		media_deadline += float(deadline)
-		media_contexto += float(contexto)
-		dados += '%s %s\n' % (deadline.rstrip(), contexto.rstrip())
+			media_deadline += float(deadline.rstrip())
+			media_contexto += float(contexto.rstrip())
+			dados += '%s,%s\n' % (deadline.rstrip(), contexto.rstrip())
 
-	dados += '%s %s\n' % (media_deadline / 30, media_contexto / 30)
+		dados += '%s,%s\n' % (media_deadline / 30, media_contexto / 30)
+		test_file = 'result_escalonador_%s_test_%s.txt' % (i, t)
 
-	test_file = 'test_result_%s.txt' % (i)
-
-	with open(test_file, 'w') as result:
-		result.write(dados)
+		with open(test_file, 'w') as result:
+			result.write(dados)
 
